27b97be5161ea626d157e33bf20d9a1d916e9793
Seyi Pythonian <prontomaster@gmail.com>
2023-06-03 04:17:11 +0100

    reversed tweet post commit

--- news/admin.py
+++ news/admin.py
@@ -17,29 +17,29 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
     ordering = ['-created']
 
-    # def change_view(self, request, object_id, extra_context=None):
-    #     """ Override the page admin is directed to after saving a post."""
-    #     result = super().change_view(request, object_id, extra_context)
-    #     post = Post.objects.get(id__iexact=object_id)
-    #     if not request.POST.__contains__('_addanother') and not request.POST.__contains__('_continue'):
-    #         result['Location'] = post.get_absolute_url()
-    #     return result
+    def change_view(self, request, object_id, extra_context=None):
+        """ Override the page admin is directed to after saving a post."""
+        result = super().change_view(request, object_id, extra_context)
+        post = Post.objects.get(id__iexact=object_id)
+        if not request.POST.__contains__('_addanother') and not request.POST.__contains__('_continue'):
+            result['Location'] = post.get_absolute_url()
+        return result
     
-    # def add_view(self, request, form_url='', extra_context=None):
-    #     # Save the new post
-    #     response = super().add_view(request, form_url, extra_context)
-    #     if isinstance(response, HttpResponseRedirect):
-    #         # Retrieve the saved post object
-    #         post = self.model.objects.latest('id')
-    #         # Construct a new redirect response with the desired URL
-    #         redirect_url = post.get_absolute_url()
-    #         response = HttpResponseRedirect(redirect_url)
-    #     return response
+    def add_view(self, request, form_url='', extra_context=None):
+        # Save the new post
+        response = super().add_view(request, form_url, extra_context)
+        if isinstance(response, HttpResponseRedirect):
+            # Retrieve the saved post object
+            post = self.model.objects.latest('id')
+            # Construct a new redirect response with the desired URL
+            redirect_url = post.get_absolute_url()
+            response = HttpResponseRedirect(redirect_url)
+        return response
 
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-        if not change:
-            post_tweet(obj)
+    # def save_model(self, request, obj, form, change):
+    #     super().save_model(request, obj, form, change)
+    #     if not change:
+    #         post_tweet(obj)
     
 
 @admin.register(Category)
